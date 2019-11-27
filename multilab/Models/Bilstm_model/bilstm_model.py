ec3f49c398af58414bd69e9971648d3e6f54ccd2
monk1337 <3x3paul@gmail.com>
2019-11-27 17:09:16 +0530

    check

--- multilab/Models/Bilstm_model/bilstm_model.py
+++ multilab/Models/Bilstm_model/bilstm_model.py
@@ -1,8 +1,16 @@
 import tensorflow as tf
 import os
 import numpy as np
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 from tensorflow.python.util import deprecation
 deprecation._PRINT_DEPRECATION_WARNINGS = False
+try:
+    from tensorflow.python.util import module_wrapper as deprecation
+except ImportError:
+    from tensorflow.python.util import deprecation_wrapper as deprecation
+deprecation._PER_MODULE_WARNING_LIMIT = 0
 
 
 class Bilstm_model(object):
